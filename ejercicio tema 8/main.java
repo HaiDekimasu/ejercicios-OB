67c7ad76ff3342c7cc5ddadee3442e5736eab077
HaiDekimasu <119706619+HaiDekimasu@users.noreply.github.com>
2022-12-05 23:27:40 -0300

    Update main.java

--- ejercicio tema 8/main.java
+++ ejercicio tema 8/main.java
@@ -1,6 +1,10 @@
 public class main {
     public static void main(String[] args) {
 
+/* 4. Crear un objeto persona en el main. */
+/* 5. Utiliza los gets y sets para darle valores a las propiedades edad, nombre
+      y telefono, por último muéstralas por consola.*/
+        
         Persona persona = new Persona();
         persona.setNombre("Jefferson");
         System.out.println("Mi nombre es " + persona.getNombre());
@@ -11,11 +15,13 @@ public class main {
     };
 };
 
+/* 1. Crear clase Persona. */
 class Persona {
+/* 2. Crear variables las privadas edad, nombre y telefono de la clase Persona.*/
     private String nombre;
     private int edad;
     private int telefono;
-
+/* 3. Crear gets y sets de cada propiedad. */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     };
