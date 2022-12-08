15cb4c0b591c3526d1e8df9654f2cabfd0b6b9d1
HaiDekimasu <119706619+HaiDekimasu@users.noreply.github.com>
2022-12-08 12:06:56 -0300

    ejercicio tema 4 Python

---
+++ EJERCICIO TEMA 4 PYTHON/ejercicio_tema_4.py
@@ -0,0 +1,6 @@
+##ejercicio_tema_4
+##Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.
+conteo_1_100 = range(1,101)
+
+for Rango in reversed(conteo_1_100):
+    print(f"- {Rango}" + " menos 1 igual a:")
